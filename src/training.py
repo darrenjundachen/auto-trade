4ea916c2f9ad786ef955e92f74185c97741c6bf3
Darren Chen <>
2021-05-17 19:55:02 +1000

    nice!!!

--- src/training.py
+++ src/training.py
@@ -64,12 +64,30 @@ def get_klines(begin, end):
     return kline_buffer[begin_index : end_index + 1]
 
 
-def simulate(current, peak_percentage, peak_period, target_profit, drop_time):
+def print_log(enabled, msg):
+    if enabled:
+        print(msg)
+
+
+def simulate(
+    current,
+    peak_percentage,
+    peak_period,
+    target_profit,
+    drop_time,
+    fee_rate=0.001,
+    ennanble_log=False,
+):
+    fees = 0
+    base_pre_sold_amount = 0
     base_currency_amount = 1.0  # start from 1
     target_currency_amount = 0
     selling_price = None
     selling_list_time = None
     exit_datetime = datetime.now()
+    profit_sold = 0
+    drop_sold = 0
+    bought = 0
 
     # One loop for one minute
     while current < exit_datetime:
@@ -79,41 +97,55 @@ def simulate(current, peak_percentage, peak_period, target_profit, drop_time):
         current_price = current_line["open"]
 
         if selling_price:
-            if current_price >= selling_price:
+            if current_price >= selling_price or (
+                current_time - selling_list_time
+            ) > timedelta(hours=drop_time):
+                is_profit_sold = current_price >= selling_price
                 base_currency_amount = target_currency_amount * current_price
                 target_currency_amount = 0
                 selling_price = None
                 selling_list_time = None
-                print(
-                    f"{Fore.GREEN}[Sold] {Fore.WHITE}{current_time} base: {base_currency_amount}, target: {target_currency_amount}, current price: {current_price}, sold price: {current_price}"
-                )
-            elif (current_time - selling_list_time) > timedelta(hours=drop_time):
-                base_currency_amount = target_currency_amount * current_price
-                target_currency_amount = 0
-                selling_price = None
-                selling_list_time = None
-                print(
-                    f"{Fore.RED}[Drop Sold] {Fore.WHITE}{current_time} base: {base_currency_amount}, target: {target_currency_amount}, current price: {current_price}, sold price: {current_price}"
-                )
+                fees += fee_rate * base_currency_amount
+
+                if is_profit_sold:
+                    profit_sold += 1
+                    print_log(
+                        ennanble_log,
+                        f"{Fore.GREEN}[Sold] {Fore.WHITE}{current_time} base: {base_currency_amount}, target: {target_currency_amount}, current price: {current_price}, sold price: {current_price}",
+                    )
+                else:
+                    drop_sold += 1
+                    print_log(
+                        ennanble_log,
+                        f"{Fore.RED}[Drop Sold] {Fore.WHITE}{current_time} base: {base_currency_amount}, target: {target_currency_amount}, current price: {current_price}, sold price: {current_price}",
+                    )
         else:
             # Get last peak_period klines
             peak_line = max(klines, key=lambda x: x["high"])
 
             # Buy current
             if (peak_line["high"] / current_line["high"]) > peak_percentage:
+                fees += fee_rate * base_currency_amount
+                base_pre_sold_amount = base_currency_amount
                 target_currency_amount = base_currency_amount / current_price
                 base_currency_amount = 0
                 selling_price = current_price * target_profit
                 selling_list_time = current_time
-                print(
+                bought += 1
+                print_log(
+                    ennanble_log,
                     Fore.WHITE
-                    + f"[Bought] {current_time} base: {base_currency_amount}, target: {target_currency_amount}, current price: {current_price}, selling price: {selling_price}"
+                    + f"[Bought] {current_time} base: {base_currency_amount}, target: {target_currency_amount}, current price: {current_price}, selling price: {selling_price}",
                 )
 
         current += timedelta(minutes=1)
 
+    total_amount = base_currency_amount or base_pre_sold_amount
+    print(f"Total Amount: {total_amount}")
+    print(f"Fees: {fees}")
+    print(Fore.GREEN + f"Final Amount: {total_amount - fees}")
+    print(Fore.WHITE + f"Sold: {profit_sold}")
+    print(f"Drop Sold: {drop_sold}")
 
-#simulate(datetime(2020, 6, 2), 1.04, 12, 1.02, 24)
-simulate(datetime(2021, 1, 1), 1.04, 12, 1.02, 24)
 
-# print(Fore.GREEN + "some red text")
+simulate(datetime(2021, 4, 1), 1.04, 12, 1.02, 48)
