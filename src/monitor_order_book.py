90ae944559a5a75e6d0453af0d478fc286bd9480
Darren Chen <>
2021-06-02 23:24:09 +1000

    money money

--- src/monitor_order_book.py
+++ src/monitor_order_book.py
@@ -1,13 +1,18 @@
 import time
 from binance_api import get_order_books
 from colorama import Fore
+import matplotlib.pyplot as plt
+from datetime import datetime
+from matplotlib.animation import FuncAnimation
+from matplotlib.dates import DateFormatter, MinuteLocator
 
-while True:
+
+def get_number():
     order_books = get_order_books(limit=5000)
-    bids = order_books['bids']
-    asks = order_books['asks']
+    bids = order_books["bids"]
+    asks = order_books["asks"]
     rate = 1.04
-    
+
     bid_total = 0
     bid_begin = float(bids[0][0])
     bid_end = bid_begin / rate
@@ -33,12 +38,39 @@ while True:
         weight_price = weight_price - ask_begin
         weight_price = weight_price ** 2
         ask_total += weight_price * amount
-    
-    result = round((bid_total - ask_total) / bid_total * 100, 2)
 
-    if result > 0:
-        print(f'{Fore.GREEN}{result}', end=' ', flush=True)
-    else:
-        print(f'{Fore.RED}{-result}', end=' ', flush=True)
+    return round((bid_total - ask_total) / bid_total * 100, 2)
 
-    time.sleep(10)
+
+items = []
+
+
+def aninmate(i):
+    items.append(
+        {"x": datetime.now(), "y": get_number()},
+    )
+
+    positive_items = [item for item in items if item["y"] >= 0]
+    negative_items = [item for item in items if item["y"] < 0]
+
+    plt.cla()
+    plt.bar(
+        [item["x"] for item in positive_items],
+        [item["y"] for item in positive_items],
+        color="g",
+        width=9 / 24 / 60 / 60,
+    )
+    plt.bar(
+        [item["x"] for item in negative_items],
+        [item["y"] for item in negative_items],
+        color="r",
+        width=9 / 24 / 60 / 60,
+    )
+    plt.gca().xaxis.set_major_formatter(DateFormatter("%H:%M:%S"))
+    plt.gca().xaxis.set_major_locator(MinuteLocator(interval=1))
+    plt.xticks(rotation=90)
+    plt.grid()
+
+
+ani = FuncAnimation(plt.gcf(), aninmate, interval=10000)
+plt.show()
